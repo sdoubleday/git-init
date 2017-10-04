2666b8f14f711e02d5d1984a9767c39e61255e99
sdoubleday <28907224+sdoubleday@users.noreply.github.com>
2017-10-04 12:54:21 -0400

    Updated tests for setup.ps1 and implemented it in git-init

--- git-init.Tests.ps1
+++ git-init.Tests.ps1
@@ -92,9 +92,18 @@ git submodule update --init --recursive
     It "setup.ps1 is created with my typical contents." {
         git-init -userEmail 'test@gmail.com' -userName 'test'
         $output = @'
-write-verbose "Setup..." -Verbose
+PARAM([Switch]$SuppressBootstrap)
+write-verbose "Setup (running $PsCommandPath)..." -Verbose
+
+If (-not $SuppressBootstrap.IsPresent) {
 write-verbose "Running bootstrap..." -Verbose
 . .\bootstrap.ps1
+}
+ELSE {write-verbose "Bootstrap suppressed." -Verbose}
+
+Get-ChildItem -Directory | Get-ChildItem -filter setup.ps1 | ForEach-Object { Push-Location; Set-Location (Split-Path -Parent $_.Fullname); . ".\$($_.Name)" -SuppressBootstrap ; Pop-Location }
+
+write-verbose "Done ($PsCommandPath)." -Verbose
 
 '@
         get-childitem setup.ps1 | get-content | Out-String | Should Be $output
